0261d715a7ec6e9d9a95c8d6a74b718c7ebceac8
sankalproy11 <sankalproy.dev@gmail.com>
2024-03-20 23:32:06 +0530

    fixed cors

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
 app.use(express.json());
 
 const uploadRoutes = require("./routes/uploadRoutes");
