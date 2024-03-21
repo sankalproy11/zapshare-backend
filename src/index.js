3ecce3d169c0a6cd710017e38ba0e3276e33b2ab
sankalproy11 <sankalproy.dev@gmail.com>
2024-03-21 15:34:13 +0530

    fixing index

--- src/index.js
+++ src/index.js
@@ -1,17 +1,13 @@
 require("dotenv").config();
 const connectDB = require("./config/mongo");
 const app = require("./app");
-const serverless = require("serverless-http");
 
 connectDB()
-  // .then(() => {
-
-  //   server.listen(process.env.PORT || 8000, () => {
-  //     console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
-  //   });
-  // })
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`⚙️ Server is running at port : ${process.env.PORT || 8000}`);
+    });
+  })
   .catch((err) => {
     console.log("MONGO db connection failed !!! ", err);
   });
-
-module.exports.handler = serverless(app);
